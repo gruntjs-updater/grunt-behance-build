b4d723df824ca1acb780694e6f10f4072d9cd3a7
Joel Kemp <jokemp@adobe.com>
2015-04-30 12:29:26 -0400

    Time Grunt: Time grunt tasks

--- tasks/aliases.js
+++ tasks/aliases.js
@@ -1,4 +1,5 @@
 module.exports = function(grunt) {
+  require('time-grunt')(grunt);
   grunt.registerTask('build', ['clean', 'unswap', 'copy:all', 'be_ugly', 'requirejs:build', 'copy:built', 'swap']);
 };
 
